d7c873832bb868b41baec263911c08f2b9c7ae61
Dong Ryu <Dong.Hyun.Ryu@raytheon.com>
2022-08-16 16:57:59 -0400

    Refactoring

--- src/main/java/com/raytheon/ldap/controller/UserController.java
+++ src/main/java/com/raytheon/ldap/controller/UserController.java
@@ -2,13 +2,16 @@ package com.raytheon.ldap.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.raytheon.ldap.auth.LdapUser;
 import com.raytheon.ldap.dto.LoginForm;
 import com.raytheon.ldap.dto.RefreshTokenRequest;
 import com.raytheon.ldap.dto.ResultForm;
@@ -31,6 +34,14 @@ public class UserController {
 		final ResultForm resultForm = userServie.login(loginForm);
 
 		return ResponseEntity.ok(resultForm);
+	}
+
+	@GetMapping("/user/{email}")
+	public ResponseEntity<LdapUser> userDetail(@PathVariable String email) {
+
+		final LdapUser ldapUser = (LdapUser) userServie.loadUserByEmail(email);
+
+		return ResponseEntity.ok(ldapUser);
 	}
 
 	@GetMapping("/test")
